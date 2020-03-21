0d6978c5de98ac462268f36f84e81f6e108c508a
KEBIR Ramdane <ramdane.kebir@outlook.fr>
2020-03-21 14:45:13 +0100

    creation et implémentation de classe

---
+++ exo3_5/src/main/java/Uvsq21923164/exo3_5/affichageconsol.java
@@ -0,0 +1,16 @@
+package Uvsq21923164.exo3_5;
+
+
+
+import java.time.LocalDateTime;
+
+public class affichageconsol implements logger {
+
+	public void affichage() {
+			System.out.println(LocalDateTime.now() + " : Dé but de uneMethodeMetier " ) ; // l o g message
+			
+			System.out.println(LocalDateTime.now() + " : Fin de uneMethodeMetier " ) ; // l o g message
+		
+	}
+
+}
